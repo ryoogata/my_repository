dc21f72b302a523cd92e5f17ddec91bc1c16360d
ryoogata <ryo.ogata@gmail.com>
2012-09-20 10:16:19 +0900

    Added the about_me cookbook

---
+++ cookbooks/about_me/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures about_me"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "rightscale"
+
+recipe "about_me::default","Print my first name and several of my favorite things as output."
